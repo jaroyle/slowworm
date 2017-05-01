24b4e721cc8383c8f6c512dee458b63c4de413eb
chrissuthy <chrissuthy@gmail.com>
2017-05-01 10:51:59 -0400

    Updated script so analysis runs on latest versions of both R and oSCR.

--- oSCR_worm_analysis_script_final_forpub.R
+++ oSCR_worm_analysis_script_final_forpub.R
@@ -1,63 +1,78 @@
-
 load("slowWorms_final.RData")
+library(oSCR)
+scrFrame <- make.scrFrame(caphist = scrFrame$caphist,
+                          traps = scrFrame$traps,
+                          indCovs = scrFrame$indCovs,
+                          trapCovs = scrFrame$trapCovs,
+                          trapOperation = scrFrame$trapOperation)
 
+plot(ssDF,scrFrame,collapse=TRUE)
 
 ss<- rbind(ssDF[[1]],ssDF[[2]],ssDF[[3]])
-
 tiff("fig1.tiff")
-plot(ss,pch= " ")
-points(ssDF[[1]],pch=".",cex=.2,col="gray")
- points(ssDF[[1]],pch=20)
- points(ssDF[[2]],pch=".",cex=.2,col="gray")
- points(ssDF[[2]],pch=20)
- points(ssDF[[3]],pch=".",cex=.2,col="gray")
- points(ssDF[[3]],pch=20)
-text(666600,200100,"area = 1")
-text(666200,199800,"area = 3")
-text(665900, 199450,"area = 2")
- dev.off()
+  plot(ss,cex=0.5,asp=1)
+  points(ssDF[[1]],pch=".",cex=.2,col="gray")
+  points(ssDF[[1]],pch=20)
+  points(ssDF[[2]],pch=".",cex=.2,col="gray")
+  points(ssDF[[2]],pch=20)
+  points(ssDF[[3]],pch=".",cex=.2,col="gray")
+  points(ssDF[[3]],pch=20)
+  text(666600,200100,"area = 1")
+  text(666200,199800,"area = 3")
+  text(665900, 199450,"area = 2")
+dev.off()
  
-library(oSCR)
-new1 <- oSCR.fit( model=list(D~1,p0~1,sig~1),scrFrame, ssDF=ssDF,start.vals=c(-3.6, log(20), -1) ,
-plotit=FALSE, multicatch=TRUE , trimS=70)
-new2 <- oSCR.fit( model=list(D~1,p0~session,sig~1),scrFrame, ssDF=ssDF,start.vals=c(-3.6, 0, 0, log(20), -1) ,
-plotit=FALSE, multicatch=TRUE, trimS=70 )
 
-new3 <- oSCR.fit( model=list(D~session,p0~1,sig~1),scrFrame, ssDF=ssDF,start.vals=c(-3.6, log(13), -3, 0, 0) ,
-plotit=FALSE, multicatch=TRUE , trimS=70)
+#sensitivity to state space resolution (3m used in paper)
+sesitivity.test <- FALSE
+if(sesitivity.test){
+  keepers <- matrix(NA,4,3)
+  ii <- 1
+  for(i in c(40,30,20,10)){
+    sss <- make.ssDF(scrFrame,buffer = 70, res = i)
+    tmp <- oSCR.fit(model=list(D~1,p0~1,sig~1), scrFrame=scrFrame, ssDF=sss,
+                    start.vals=c(-3.6, log(20), -1), multicatch=TRUE, trimS=70)
+    keepers[ii,] <- tmp$outStats$mle
+    ii <- ii+1
+  }
+}
 
-new4 <- oSCR.fit( model=list(D~session,p0~session,sig~1),scrFrame, ssDF=ssDF,start.vals=c(-2.9, -0.5, -1, log(13), -3, 0, 0) ,
-plotit=FALSE, multicatch=TRUE, trimS=70 )
+#fit the models
+new1 <- oSCR.fit(model=list(D~1,p0~1,sig~1), scrFrame=scrFrame, ssDF=ssDF,
+                 start.vals=c(-3.6, log(20), -1), multicatch=TRUE, trimS=70)
 
-new5 <- oSCR.fit( model=list(D~session,p0~session + doy,sig~1),scrFrame, ssDF=ssDF,start.vals=c(-2.9, -0.5, -1, log(13)
-,0, -4, 1.2, 1.2) ,
-plotit=FALSE, multicatch=TRUE , trimS=70)
+new2 <- oSCR.fit(model=list(D~1,p0~session,sig~1), scrFrame=scrFrame, ssDF=ssDF,
+                 start.vals=c(-3.6, 0, 0, log(20), -1), multicatch=TRUE, trimS=70)
 
-new6 <- oSCR.fit( model=list(D~session,p0~session + doy + I(doy^2),sig~1),scrFrame, ssDF=ssDF,
-start.vals=c(-2.4, -1.2, -1.9, log(13)
-,0,0, -4, 1.2, 1.2) ,
-plotit=FALSE, multicatch=TRUE , trimS=70)
+new3 <- oSCR.fit(model=list(D~session,p0~1,sig~1), scrFrame, ssDF=ssDF, 
+                 start.vals=c(-3.6, log(13), -3, 0, 0) , multicatch=TRUE, trimS=70)
 
-new7 <- oSCR.fit( model=list(D~session,p0~1 + doy  ,sig~1),scrFrame, ssDF=ssDF,
-start.vals=c(-3.4, log(13)
-,0, -4, 1.2, 1.2) ,
-plotit=FALSE, multicatch=TRUE, trimS=70 )
+new4 <- oSCR.fit(model=list(D~session,p0~session,sig~1), scrFrame, ssDF=ssDF,
+                 start.vals=c(-2.9, -0.5, -1, log(13), -3, 0, 0), multicatch=TRUE, trimS=70 )
 
-new8 <- oSCR.fit( model=list(D~session,p0~1 + doy + I(doy^2)  ,sig~1),scrFrame, ssDF=ssDF,
-start.vals=c(-3.4, log(13),0,0, -4, 0.8, 0.5), 
-plotit=FALSE, multicatch=TRUE, trimS=70 )
+new5 <- oSCR.fit(model=list(D~session,p0~session + doy,sig~1), scrFrame, ssDF=ssDF,
+                 start.vals=c(-2.9, -0.5, -1, log(13),0, -4, 1.2, 1.2), multicatch=TRUE, trimS=70)
 
-fl<-fitList.oSCR(list(new1,new2,new3,new4,new5,new6,new7,new8),rename=TRUE)
-modSel.oSCR(fl) -> ms
+new6 <- oSCR.fit(model=list(D~session,p0~session + doy + I(doy^2),sig~1), scrFrame, ssDF=ssDF,
+                 start.vals=c(-2.4, -1.2, -1.9, log(13),0,0, -4, 1.2, 1.2), multicatch=TRUE , trimS=70)
 
-save(new1,new2,new3,new4,new5,new6,new7,new8,file="finalmodels.RData")
+new7 <- oSCR.fit(model=list(D~session,p0~1 + doy,sig~1), scrFrame, ssDF=ssDF,
+                 start.vals=c(-3.4, log(13),0, -4, 1.2, 1.2), multicatch=TRUE, trimS=70 )
+
+new8 <- oSCR.fit(model=list(D~session,p0~1 + doy + I(doy^2),sig~1), scrFrame, ssDF=ssDF,
+                 start.vals=c(-3.4, log(13),0,0, -4, 0.8, 0.5), multicatch=TRUE, trimS=70 )
+
+fl <- fitList.oSCR(list(new1, new2, new3, new4, new5, new6, new7, new8), rename=TRUE)
+ms <- modSel.oSCR(fl)
+
+save(new1, new2, new3, new4, new5, new6, new7, new8, file="finalmodelsCS.RData")
 
 library(msm)
-mn<-new6$rawOutput$estimate[1:3]
-cov<- solve(new6$rawOutput$hessian[1:3, 1:3])
+mn <- new6$rawOutput$estimate[1:3]
+cov <- solve(new6$rawOutput$hessian[1:3, 1:3])
 c(plogis(mn[1]), plogis(mn[1]+mn[2]), plogis(mn[1]+mn[3]) )
-deltamethod( list(~exp(x1)/(1+exp(x1)), ~exp(x1+x2)/(1+exp(x1+x2)), ~exp(x1+x3)/(1+exp(x1+x3))),
- mean=mn, cov=cov)
+deltamethod(list(~exp(x1)/(1+exp(x1)), ~exp(x1+x2)/(1+exp(x1+x2)), ~exp(x1+x3)/(1+exp(x1+x3))),
+            mean=mn, cov=cov)
 
 mn<-new6$rawOutput$estimate[7:9]
 cov<- solve(new6$rawOutput$hessian[7:9, 7:9])
